3ba4f057e4c0a2f4d9d1fe0eeab59ba35eb6bb0a
Sameer <98used@gmail.com>
2024-02-02 23:34:27 +0530

    first react and vite

---
+++ basicreact01/src/App.js
@@ -0,0 +1,15 @@
+
+
+function App() {
+  return (
+  <h3><b><i>Hello world|| first install node js in system then open a folder in vs code then run the command
+  npm create-react-app to create a react app which gives the not so basic react app which can be run with command npm start
+  with vite  npm create vite@latest then installs necessary libraries give projectname select framework with arrow keys
+  as react then variant as javascript created then to run the vite we need nodemodules for which run npm i command 
+  then npm run dev ioiiooi</i></b>
+  </h3>
+  
+  );
+}
+
+export default App;
